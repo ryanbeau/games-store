9a1b754a011d494287ff1b857ae7d28155bff6e2
RyanTimothy <39531128+RyanTimothy@users.noreply.github.com>
2020-10-08 01:23:31 -0400

    Readd for gitignore

---
+++ Sprint/Sprint/Models/GameType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Sprint.Models
+{
+    public partial class GameType
+    {
+        public GameType()
+        {
+            Games = new HashSet<Game>();
+        }
+
+        public int GameTypeId { get; set; }
+        public string Type { get; set; }
+
+        public virtual ICollection<Game> Games { get; set; }
+    }
+}
