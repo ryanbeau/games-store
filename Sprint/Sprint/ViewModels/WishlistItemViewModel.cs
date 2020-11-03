402b8c9228d6ec9bb2380811cb0b1b67391ff1a6
RyanTimothy <39531128+RyanTimothy@users.noreply.github.com>
2020-11-03 12:10:14 -0500

    Update WishlistViewModel with new WishtlistItemViewModel

---
+++ Sprint/Sprint/ViewModels/WishlistItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Sprint.Models;
+
+namespace Sprint.ViewModels
+{
+    public class WishlistItemViewModel
+    {
+        public bool IsInCart { get; set; }
+        public GameImage Image { get; set; }
+
+        public UserGameWishlist WishlistItem { get; set; }
+    }
+}
