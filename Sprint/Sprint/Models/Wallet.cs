58016f0ef13357fe1b86f71eafbfbaea43b92b2e
Conor Shea <Cshea7041@conestogac.on.ca>
2020-11-20 15:14:39 -0500

    address and wallet models and controllers

---
+++ Sprint/Sprint/Models/Wallet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sprint.Models
+{
+    public class Wallet
+    {
+        public int WalletId { get; set; }
+
+        public int UserId {get; set;}
+
+        public int CardNumber { get; set; }
+
+        public int Month { get; set; }
+
+        public int Year { get; set; }
+
+        public User User { get; set; }
+
+    }
+}
